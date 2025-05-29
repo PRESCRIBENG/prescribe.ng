c496fc2b574591889995ac97cc151e8aa25e3f17
Chimdi <160367531+chimdi007@users.noreply.github.com>
2025-05-29 13:36:23 +0100

    Create route.ts

---
+++ src/app/api/web/pre_signup/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    
+    const response = await fetch('https://gelataskia.prescribe.ng/web/pre_signup', {
+    //const response = await fetch('http://192.168.1.144:5002/web/pre_signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    
+    const contentType = response.headers.get('content-type');
+    const raw = await response.text();
+    
+    if (!contentType || !contentType.includes('application/json')) {
+      console.error('Unexpected response:', raw);
+      return NextResponse.json(
+        { message: 'Unexpected response from backend', raw },
+        { status: 500 }
+      );
+    }
+    
+    const data = JSON.parse(raw);
+    return NextResponse.json(data, { status: response.status });
+    
+  
+  } catch (error) {
+    console.error('Login API error:', error);
+    return NextResponse.json(
+      { message: 'An error occurred while processing your request' },
+      { status: 500 }
+    );
+  }
+}
